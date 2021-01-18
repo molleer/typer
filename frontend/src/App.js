6d5221e74bb922addf28eefd090f9339602e0ea9
David Möller <damoller99@hotmail.se>
2021-01-18 19:54:26 +0100

    Made first type version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 import TextField from "./components/textfield";
+import Word from "./components/word";
+
+const text =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 const App = () => {
+  const [words, setWords] = useState(
+    [...text.toLowerCase()]
+      .filter(e => e.match(/[a-z+A-Z ]/))
+      .join("")
+      .split(" ")
+      .map(e => ({ expected: e, got: "" })),
+  );
+  const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+
+  const space = () => {
+    const tmp = [...words];
+    tmp[count].got = input;
+    tmp[count].done = true;
+    setWords(tmp);
+    setInput("");
+    setCount(count + 1);
+  };
+
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
         alignContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
+      <div style={{ display: "flex", width: "400px", flexWrap: "wrap" }}>
+        {words.map((word, i) => (
+          <Word
+            key={i}
+            {...word}
+            current={i === count}
+            got={i === count ? input : word.got}
+          />
+        ))}
+      </div>
       <TextField
         value={input}
         onChange={value => {
-          if (value.endsWith(" ")) setInput("");
+          if (value.endsWith(" ")) space();
           else setInput(value);
         }}
       />
